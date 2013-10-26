1cdeb77d65929abdd7f737f47fb81d16cfbd94f4
Abhinay_Kryptcoder <Abhinay_Kryptcoder@user-PC>
2013-10-26 17:14:46 +0530

    Added AdminHome and its Controller

---
+++ Quotations/src/com/kc/controller/AdminHomeController.java
@@ -0,0 +1,81 @@
+package com.kc.controller;
+
+public class AdminHomeController {
+
+	public void priceEstimation()
+	{
+		
+	}
+	public void quotationPreparation()
+	{
+		
+	}
+	public void salesOrder()
+	{
+		
+	}
+	public void status()
+	{
+		
+	}
+	public void reports()
+	{
+		
+	}
+	public void createProduct()
+	{
+		
+	}
+	public void modifyProduct()
+	{
+		
+	}
+	public void viewProduct()
+	{
+		
+	}
+	public void createComponent()
+	{
+		
+	}
+	public void modifyComponent()
+	{
+		
+	}
+	public void viewComponent()
+	{
+		
+	}
+	public void createCustomer()
+	{
+		
+	}
+	public void modifyCustomer()
+	{
+		
+	}
+	public void viewCustomer()
+	{
+		
+	}
+	public void createUser()
+	{
+		
+	}
+	public void modifyUser()
+	{
+		
+	}
+	public void viewUser()
+	{
+		
+	}
+	public void companyDetails()
+	{
+		
+	}
+	public void about()
+	{
+		
+	}	
+}
