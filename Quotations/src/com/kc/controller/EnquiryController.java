43ca322e6c73be988a9eaa7ecbca42f9106dea66
panther999 <pragatirpatra@gmail.com>
2013-12-02 21:42:59 +0530

    Enquiry Controller CheckIn

--- Quotations/src/com/kc/controller/EnquiryController.java
+++ Quotations/src/com/kc/controller/EnquiryController.java
@@ -3,18 +3,18 @@ package com.kc.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-import javafx.collections.ObservableList;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 
-import com.kc.dao.ProductsDAO;
-import com.kc.model.ProductsVO;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class EnquiryController implements Initializable {
 	private static final Logger LOG = LogManager.getLogger(EnquiryNewController.class);
@@ -24,21 +24,49 @@ public class EnquiryController implements Initializable {
 	private Tab viewTab;
 	@FXML
 	private Tab optionTab;
+	@FXML
+	private TabPane tabPane;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		LOG.info("Enter : initialize");
-		try{
+		try{			
 			
 			FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/enquiry-new.fxml"));
 			BorderPane newEnquiry = (BorderPane) loader.load();
 			newTab.setContent(newEnquiry);
-			FXMLLoader loader2 = new FXMLLoader(this.getClass().getResource("../view/enquiry-view.fxml"));
-			BorderPane viewEnquiry = (BorderPane) loader2.load();
-			viewTab.setContent(viewEnquiry);
-			FXMLLoader loader3 = new FXMLLoader(this.getClass().getResource("../view/enquiry-options.fxml"));
-			BorderPane optionsEnquiry = (BorderPane) loader3.load();
-			optionTab.setContent(optionsEnquiry);
+			
+			tabPane.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
+			
+		        @Override
+		        public void changed(ObservableValue<? extends Tab> ov, Tab t, Tab t1) {
+					try{
+					if(t1.equals(newTab))
+					{
+						FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/enquiry-new.fxml"));
+						BorderPane newEnquiry = (BorderPane) loader.load();
+						newTab.setContent(newEnquiry);
+					}
+					else if(t1.equals(viewTab))
+					{
+						FXMLLoader loader2 = new FXMLLoader(this.getClass().getResource("../view/enquiry-view.fxml"));
+						BorderPane viewEnquiry = (BorderPane) loader2.load();
+						viewTab.setContent(viewEnquiry);
+					}
+					else
+					{
+						FXMLLoader loader3 = new FXMLLoader(this.getClass().getResource("../view/enquiry-options.fxml"));
+						BorderPane optionsEnquiry = (BorderPane) loader3.load();
+						optionTab.setContent(optionsEnquiry);
+					}
+					
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				}				
+			});
 		}
 		catch (Exception e) {
 			// TODO: handle exception
