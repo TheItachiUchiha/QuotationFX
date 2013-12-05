ac5b773629ae803d87dba6ca4a524105d4e0e0d9
panther999 <pragatirpatra@gmail.com>
2013-12-05 22:18:11 +0530

    Adding java mail api jar and encryption/decryption

---
+++ Quotations/src/com/kc/util/Email.java
@@ -0,0 +1,91 @@
+package com.kc.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javafx.concurrent.Task;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import com.kc.constant.CommonConstants;
+import com.kc.dao.EnquiryDAO;
+
+public class Email extends Task
+{
+	EnquiryDAO enquiryDAO = new EnquiryDAO();
+	Encryption encryption = new Encryption("");
+	Map<String, String> data = new HashMap<String, String>();
+	
+	public Email(Map<String, String> data)
+	{
+		this.data = data;
+	}
+	public void sendEmailFromGmail(Map<String, String> data)
+	{
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		try {
+ 
+			final Map<String, String> map = enquiryDAO.getEnquiryOptionDefaultValues();
+			Session session = Session.getDefaultInstance(props,
+					new javax.mail.Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(map.get(CommonConstants.KEY_ENQUIRY_EMAIL_USERNAME), encryption.decrypt(map.get(CommonConstants.KEY_ENQUIRY_EMAIL_PASSWORD)));
+						}
+					});
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("support@kryptcode.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(data.get(CommonConstants.EMAIL_TO)));
+			message.setSubject("Testing Subject");
+			// create the message part   
+            MimeBodyPart messageBodyPart =   
+              new MimeBodyPart();  
+        //fill message  
+            messageBodyPart.setText(data.get(CommonConstants.EMAIL_BODY));  
+            Multipart multipart = new MimeMultipart();  
+            multipart.addBodyPart(messageBodyPart);  
+         // Part two is attachment  
+            messageBodyPart = new MimeBodyPart();  
+            DataSource source =   
+              new FileDataSource(data.get(CommonConstants.EMAIL_ATTACHMENT));  
+            messageBodyPart.setDataHandler(  
+              new DataHandler(source));  
+            messageBodyPart.setFileName(data.get(CommonConstants.EMAIL_ATTACHMENT));  
+            multipart.addBodyPart(messageBodyPart);  
+        // Put parts in message  
+            message.setContent(multipart);  
+ 
+			Transport.send(message);
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected Object call() throws Exception {
+		// TODO Auto-generated method stub
+		sendEmailFromGmail(this.data);
+		return null;
+	}
+}
